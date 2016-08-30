6692843c65a909590d34ae3749862e9f1e9ad318
Aprijal <iskandaraprijal@gmail.com>
2016-08-30 11:14:54 +0700

    Update PRAKTIKUM_MIKRO 2.3.ino

--- PRAKTIKUM_MIKRO 2.3.ino
+++ PRAKTIKUM_MIKRO 2.3.ino
@@ -1,10 +1,10 @@
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
 pinMode(12,OUTPUT );
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
 digitalWrite(12,HIGH);
 delay(500);
 digitalWrite(12,LOW);
